5a3609020370ebde991ffa618ccfedf41d89650b
Seyyed AmirAli <mashroty@gmail.com>
2020-12-23 17:12:29 +0330

    main update

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 #include "I2CKeyPad.h"
 #include <LiquidCrystal_I2C.h>
 #include <string.h>
-#include <pins_arduino.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h> 
 #include <ESP8266WebServer.h>
@@ -13,6 +12,17 @@
 #include <WiFiManager.h>  
 #include <ArduinoJson.h>
 
+static const uint8_t D0   = 16;
+static const uint8_t D1   = 5;
+static const uint8_t D2   = 4;
+static const uint8_t D3   = 0;
+static const uint8_t D4   = 2;
+static const uint8_t D5   = 14;
+static const uint8_t D6   = 12;
+static const uint8_t D7   = 13;
+static const uint8_t D8   = 15;
+static const uint8_t D9   = 3;
+static const uint8_t D10  = 1;
 
 #define cls             10
 #define up              11
